3a02dafffdbb17a2e3912f583cd2b4135424a86b
23Shivani <shivani.gharve.2310@gmail.com>
2021-07-10 18:22:34 +0530

    Added: Prime Number Program

---
+++ src/com/bridgelabz/primenumber/PrimeNumber.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.primenumber;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int n = sc.nextInt();
+		int ans = 0;
+		for (int i = 2; i<=n/2;i++) {
+			ans = n%i;
+		}
+		if (ans == 0) {
+			System.out.println(n+" is not Prime number");
+		}
+		else {
+			System.out.println(n+" is a Prime number");
+		}
+	}
+
+	}
